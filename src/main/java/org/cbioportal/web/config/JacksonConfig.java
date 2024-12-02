985e3a5d68058abb4f6f5224ce91eea7839849ec
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2024-12-02 15:01:38 -0600

    Update Jackson Max String Length

--- src/main/java/org/cbioportal/web/config/JacksonConfig.java
+++ src/main/java/org/cbioportal/web/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package org.cbioportal.web.config;
 
+import com.fasterxml.jackson.core.StreamReadConstraints;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,13 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JacksonConfig {
+	static {
+        
+		StreamReadConstraints.overrideDefaultStreamReadConstraints(
+            StreamReadConstraints.builder().maxStringLength(100000000).build()
+        );
+    }
+	
     @Bean
     @Primary
     public ObjectMapper objectMapper() {
